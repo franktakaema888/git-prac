a70f7f1d8f19c61cca88d882e68570358e59253e
RedeyeXDX <mrbrandonpeh@gmail.com>
2025-03-08 11:18:58 +0800

    brandon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    add();
+  }, []);
+
+  const add = () => {
+    for (let i = 0; i < 10000000; i++) {
+      console.log("add........");
+    }
+  };
 
   return (
     <>
